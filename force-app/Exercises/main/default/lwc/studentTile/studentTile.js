b6cbb6da1a6fc572d7a5d515dc7ce78a1bd45325
wldms2494 <wldms2494@gmail.com>
2022-03-21 16:06:24 +0900

    Exercise 2-5 Completed

--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -10,4 +10,8 @@ export default class StudentTile extends LightningElement {
     get tileSelected(){
         return this.selected ? "tile selected" : "tile";
     }
+
+    studentClick(){
+        alert(this.student.Name);
+    }
 }
