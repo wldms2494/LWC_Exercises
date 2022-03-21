a663530b481ee90e71e0f91a537360b4b0875f56
wldms2494 <wldms2494@gmail.com>
2022-03-21 15:47:54 +0900

    Exercise 2-5 Completed

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,3 +1,19 @@
 import { LightningElement } from 'lwc';
 
-export default class StudentBrowser extends LightningElement {}
+export default class StudentBrowser extends LightningElement {
+    studentList = [];
+    constructor() {
+        super();
+        const studentNames =  ['Rad', 'Stuart', 'Andres', 'Rahul', 'Amit', 'Simon'];
+        this.studentList = studentNames.map((studentNames, index) =>{
+            return{
+                'sobjectType' : 'Contact',
+                'Name' : studentNames,
+                'PhotoUrl' :'/services/images/photo/003B0FakePictId',
+                'Id' : index
+            };
+        });
+    }
+
+    
+}
