6ed992d89c85d51a1dcca92d2e24c9006f78c501
wldms2494 <wldms2494@gmail.com>
2022-03-21 15:06:29 +0900

    Exercise 2-4 Completed

---
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -0,0 +1,13 @@
+import { LightningElement , api} from 'lwc';
+
+export default class StudentTile extends LightningElement {
+    @api student ={
+        Name:"jinny",
+        PhotoUrl:'/services/images/photo/003B0FakePictId',
+    };
+    @api isSelected = false;
+
+    get tileSelected(){
+        return this.selected ? "tile selected" : "tile";
+    }
+}
