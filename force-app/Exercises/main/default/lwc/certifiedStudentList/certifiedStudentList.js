0c383fd44d77e072f52e35214063459e8113c799
wldms2494 <wldms2494@gmail.com>
2022-03-24 14:25:40 +0900

    Exercise 5-5 English

--- force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
+++ force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
@@ -3,6 +3,7 @@ import getCertifiedStudents from '@salesforce/apex/CertifiedStudentList.getCerti
 import deleteStudentCertification from '@salesforce/apex/CertifiedStudentList.deleteStudentCertification';
 import {refreshApex} from '@salesforce/apex';
 import Utils from 'c/utils';
+import LABEL_FEATURE_NOT_AVAILABLE from '@salesforce/label/c.Feature_Not_Available';
 export default class CertifiedStudentList extends LightningElement {
 
     @api certificationId=0;
@@ -14,7 +15,7 @@ export default class CertifiedStudentList extends LightningElement {
 
     notAvailable(){
 
-        Utils.showModal(this,'Not Available','This feature is currently unavailable');
+        Utils.showModal(this,'Not Available',LABEL_FEATURE_NOT_AVAILABLE);
     }
 
     getSelectedIDs(){
