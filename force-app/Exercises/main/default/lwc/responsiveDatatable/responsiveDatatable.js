33fb4c55b3cf5aa4ca555c05c8115ad3c52b0957
wldms2494 <wldms2494@gmail.com>
2022-03-24 10:49:02 +0900

    Exercise 5-2 Kind of works

--- force-app/Exercises/main/default/lwc/responsiveDatatable/responsiveDatatable.js
+++ force-app/Exercises/main/default/lwc/responsiveDatatable/responsiveDatatable.js
@@ -20,6 +20,11 @@ export default class ResponsiveDatatable extends LightningElement {
 		}
 	}
 	
+	@api setSelectedRecord(recordId){
+		const mySelector = `tr[data-pk='${recordId}']`;
+		const selectedRow = this.template.querySelector(mySelector);
+		if(selectedRow){this.highlightSelectedRow(selectedRow);}
+	}
 
 	highlightSelectedRow(target){
 		if(this._selectedRow)
